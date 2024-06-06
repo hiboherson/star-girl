dc8d56e5461718464dd90acde2d816a0f7554a53
hiboherson <hiboherson@gmail.com>
2024-06-06 17:02:33 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+//const heading = document.createElement("h1");
+//heading.textContent = "Dom manipulation";
+// document.body.append(heading)
+//heading.style.color = "red";
+//heading.style.fontSize = "60";
+
+
+
+//const array  = [ "apple","mango","lemon","avocado","ibro"]
+//const orderedList = document.createElement("ol")
+//document.body.append(orderedList)
+//for (i=0 ; i< array.length ; i++) {
+//let list =document.createElement("li")
+//list.textContent = array[i]
+    //orderedList.append(list)
+//}
+
+//document.addEventListener("DOMContentLoaded", function() {
+    //const button = document.getElementById("myButton");
+    //button.addEventListener("click", function() {
+       // alert("Button clicked!");
+    //});
+//});
